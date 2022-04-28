7290b4f0d355f2ef4467e5089a680f25c25e2604
flyingisfreedom5 <berkay_tor@hotmail.com>
2022-04-29 01:54:54 +0300

    Final CSS Commit

--- models/place.js
+++ models/place.js
@@ -10,7 +10,7 @@ const commentSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User', required:true},
     userName: String,
     userAvatar: String
-},{
+},  {
     timestamps: true
 });
 
